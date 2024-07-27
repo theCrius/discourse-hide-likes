2d60e79b4aab5e942de5929f7abdcc97320154a2
Crius <claudio.vallesi@gmail.com>
2024-07-27 12:33:47 +0100

    add hideForSilenced to check

--- javascripts/discourse/initializers/init-hide-likes.js
+++ javascripts/discourse/initializers/init-hide-likes.js
@@ -1,9 +1,7 @@
 import { apiInitializer } from "discourse/lib/api";
 export default apiInitializer("0.8", (api) => {
   api.reopenWidget("post-menu", {
-    menuItems() {
-      if (this.currentUser) console.log(this.currentUser)
-        
+    menuItems() {        
       const hideForNewUser  = settings.hide_from_new_users &&
                               this.currentUser &&
                               this.currentUser.trust_level === 0;
@@ -14,7 +12,7 @@ export default apiInitializer("0.8", (api) => {
                               this.currentUser &&
                               this.currentUser.silenced === true
       
-      if (hideForNewUser || hideForAnon) {
+      if (hideForNewUser || hideForAnon || hideForSilenced) {
         return this.siteSettings.post_menu
           .split("|")
           .filter((item) => item !== "like");
