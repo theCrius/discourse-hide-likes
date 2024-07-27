922c0c926a02eaedde08d54deab915e8f764b2b4
Crius <claudio.vallesi@gmail.com>
2024-07-27 12:20:39 +0100

    add silenced user logic

--- javascripts/discourse/initializers/init-hide-likes.js
+++ javascripts/discourse/initializers/init-hide-likes.js
@@ -2,12 +2,16 @@ import { apiInitializer } from "discourse/lib/api";
 export default apiInitializer("0.8", (api) => {
   api.reopenWidget("post-menu", {
     menuItems() {
-      const hideForNewUser =
-        settings.hide_from_new_users &&
-        this.currentUser &&
-        this.currentUser.trust_level === 0;
-      const hideForAnon = settings.hide_from_anons && !this.currentUser;
+      const hideForNewUser  = settings.hide_from_new_users &&
+                              this.currentUser &&
+                              this.currentUser.trust_level === 0;
+      
+      const hideForAnon     = settings.hide_from_anons && !this.currentUser;
 
+      const hideForSilenced = settings.hide_from_silenced &&
+                              this.currentUser &&
+                              this.currentUser.silenced === true
+      
       if (hideForNewUser || hideForAnon) {
         return this.siteSettings.post_menu
           .split("|")
